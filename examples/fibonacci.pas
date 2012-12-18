349d55b1ce035dac9f8ef9f203405ec9b026a8b4
Zakhar Pilipchuk <z.pilipchuk@gmail.com>
2012-12-18 15:53:23 +0400

    Better ifs

---
+++ examples/fibonacci.pas
@@ -0,0 +1,21 @@
+program fibonacci;
+
+function fib(n : integer) : integer
+begin
+    if n = 0 then begin
+        0
+    end
+    else begin
+        if n = 1 then begin
+            1
+        end
+        else begin
+            fib(n - 2) + fib(n - 1)
+        end
+    end
+    
+end;
+
+begin
+    fib(30)
+end
